76cc75c1c5d4da41cdeb5c2687253e1a7a92288a
Victor Axelsson <victor_axelsson@hotmail.com>
2014-11-03 22:22:11 +0100

    Created headerfile for libpower and created a suggestion to a main program. Note that this is only intended as a suggestion and contains pseudo code. Also, see compiler info in main.c that could be used as a base for a makefile.

---
+++ libpower.h
@@ -0,0 +1,5 @@
+#ifndef LIBPOWER_H_INCLUDED
+#define LIBPOWER_H_INCLUDED
+float calc_power_r(float volt, float resistance);
+float calc_power_i(float volt, float current);  
+#endif
---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "libpower.h"
+#include "resistancecalc.h"
+//#include "tredjebiblioteket.h"
+
+//program can be compiled with:
+//     gcc main.c libpower.c resistancecalc.c -lm main
+//the -lm flag is because of the math lib. Don't know why but it won't compile 
+//otherwise. 
+
+
+int main(int argc, char *argv[])
+{
+
+  //Hämta späningskälla (volt för de som inte pratar ström)
+  //Hämta kopplingstyp {P | S}
+  //Hämta antal komponenter = count
+  
+  //loopa på count
+  
+     //hämta resstansen (ohm för de som inte pratar ström)
+     //stoppa in variabeln i en float array
+
+  //end loop
+  
+  //Här bör all hämtning vara gjord. Därefter kan man köra de 
+  //metoder som vi skrivit
+
+  //Ersättningsresistans = calc_resstance()
+
+  return 1; 
+}
