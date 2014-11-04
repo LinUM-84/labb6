da26d60164a747697f731b2b002be0fc24e88067
Fredric Luthman <luthman@gmail.com>
2014-11-04 03:34:18 -0800

    Working prototype with 8 test cases

--- resistancecalc.c
+++ resistancecalc.c
@@ -2,15 +2,26 @@
 #include <stdio.h>
 #include <float.h>
 #include "resistancecalc.h"
+/*#define DEBUG 0*/
 
 float calc_resistance ( int count, char conn, float *array ) {
 
 	/* Process the parameters to decide which connection method to use */
 	/* Check that we have more than zero components and a valid value for series or parallell connection */
 
+	if (!array) {
+		#ifdef DEBUG		
+		printf("Debug: Zero pointer array\n");
+		#endif
+		return -1;
+
+	}
+
 	if ((count<2) || (conn != 'P' && conn != 'S')) {
 		
+		#ifdef DEBUG
 		printf("Debug: count=%d, conn=%c", count, conn);
+		#endif
 		return -1; /* Return the value of -1 to indicate erroneous input parameters */
 
 	}
@@ -45,27 +56,35 @@ float calc_resistance ( int count, char conn, float *array ) {
 				return 0;
 
 			if (isFirst) { /* Check if we are processing the first resistance */
+				#ifdef DEBUG				
 				printf("Debug: First if true at iteration %d\n", i);
+				#endif
 				nxtResistance = array[i+1];
 				lastResistance = add_paralell(curResistance, nxtResistance);
-				//i++; /* Pad the counter to offset for paralell addition */
+				
 				
 			}
 
 			else if (!isLast) { /* Check if we are processing a resistance which is neither the first nor the last */
+				#ifdef DEBUG
 				printf("Debug: Second if true at iteration %d\n", i);
+				#endif
 				nxtResistance = array[i+1];
 				lastResistance = add_paralell(lastResistance, nxtResistance);
 
 			}
 
 			else if (isLast) { /* Check if we are processing the last resistance */
+				#ifdef DEBUG
 				printf("Debug: Third if true at iteration %d\n", i);
+				#endif
 				resultingResistance = lastResistance;
 
 			}
 
+			#ifdef DEBUG			
 			printf("Debug: conn=%c, i=%d, lastResistance=%f, curResistance=%f, nxtResistance=%f, resultingResistance=%f\n", conn, i, lastResistance, curResistance, nxtResistance, resultingResistance);
+			#endif
 			
 			break;
 
@@ -78,7 +97,9 @@ float calc_resistance ( int count, char conn, float *array ) {
 		isFirst = false; /* Set the isFirst flag to false after first loop */
 	}
 
+	#ifdef DEBUG	
 	printf("Debug: conn=%c, i=%d, maxResistance=%f, minResistance=%f, resultingResistance=%f\n", conn, i, maxResistance, minResistance, resultingResistance);
+	#endif
 
 	/* Sanity check */
 	/* For resistors connected in paralell the resulting sum must be less than the smallest ingoing resistor */
@@ -88,7 +109,7 @@ float calc_resistance ( int count, char conn, float *array ) {
 			
 	}
 
-	else {
+	else if (conn == 'P' && resultingResistance > minResistance){
 
 		return -1; /* Return -1 indicating an error */
 
@@ -101,7 +122,7 @@ float calc_resistance ( int count, char conn, float *array ) {
 			
 	}
 
-	else {
+	else if (conn == 'S' && resultingResistance < maxResistance){
 
 		return -1; /* Return -1 indicating an error */
 
--- test_rescalc.c
+++ test_rescalc.c
@@ -1,53 +1,138 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#include <math.h>
 #include "resistancecalc.h"
+#include "test_rescalc.h"
 
 /* Define the maximum allowable length for data entry */
 #define MAX_STRING_LENGTH 512
 
 int main ( int argc, char* argv[] ) {
 	
-	/* Allocate memory for the string of numbers 
-	float *array = malloc (10);
-	if (array == NULL) {
 
-		printf("No memory available\n");
-		return 1;
+	runTestCaseScenarios();
+	return 0;
 
-	}*/
+}
 
-	//oat array[10] = {5,10,15,20,25,30,35,40,45,50};
-	int count = 61;
+void runTestCaseScenarios() {
 
-	/*float array[85] =	{1, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2,
-				 10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82,
-				 100, 120, 150, 180, 220, 270, 330, 390, 470, 560, 680, 820,
-				 1000, 1200, 1500, 1800, 2200, 2700, 3300, 3900, 4700, 5600, 6800, 8200,
-				 10e3, 12e3, 15e3, 18e3, 22e3, 27e3, 33e3, 39e3, 47e3, 56e3, 68e3, 82e3,
-				 10e4, 12e4, 15e4, 18e4, 22e4, 27e4, 33e4, 39e4, 47e4, 56e4, 68e4, 82e4,
-				 10e5, 12e5, 15e5, 18e5, 22e5, 27e5, 33e5, 39e5, 47e5, 56e5, 68e5, 82e5, 
-				 10e6};*/
+	int tcCount = 8;
+	int tcStatus[tcCount];
+
+	/* Test case 1: zero pointer */
+	/* Expected result: -1 */
+	float expectedResult = -1;
+	float *zeroarray = 0;
+	int count = 10;
+	char conn = 'P';
+	int tc = 1;
+	char *tcDesc = "Zero pointer";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, zeroarray);
+	
+	/* Test case 2: Ten resistors connected in series where R=5*x and 0<x<=10 */
+	expectedResult = 275;
+	float array[10] = {5,10,15,20,25,30,35,40,45,50};
+	count = 10;
+	conn = 'S';
+	tc = 2;
+	tcDesc = "Ten resistors in series where R=5*x and 0<x<=10";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array);
+
+	/* Test case 3: Ten resistors connected in paralell where R=5*x and 0<x<=10 */
+	expectedResult = 1.707086;
+	count = 10;
+	conn = 'P';
+	tc = 3;
+	tcDesc = "Ten resistors in paralell where R=5*x and 0<x<=10";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array);
+
+	/* Test case 4: Paralell connection with a zero ohm resistor */
+	expectedResult = 0;	
+	float array2[10] = {5,10,15,0,25,30,35,40,45,50};
+	count = 10;
+	conn = 'P';
+	tc = 4;
+	tcDesc = "Paralell connection with a zero ohm resistor";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array2);
+
+	/* Test case 5: Incorrect parameters - connection mode*/
+	expectedResult = -1;	
+	count = 10;
+	conn = 'A';
+	tc = 5;
+	tcDesc = "Incorrect parameters - connection mode";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array);
+
+	/* Test case 6: Incorrect parameters - number of components */
+	expectedResult = -1;	
+	float array3[1] = {5};
+	count = 1;
+	conn = 'P';
+	tc = 6;
+	tcDesc = "Incorrect parameters - number of components";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array3);
 
 
-	float array[61] =	{10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82,
+	/* Test case 7: entire E12 resistor list connected in series*/
+	expectedResult = 5766619;
+	float array4[61] =	{10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82,
 				 100, 120, 150, 180, 220, 270, 330, 390, 470, 560, 680, 820,
 				 1000, 1200, 1500, 1800, 2200, 2700, 3300, 3900, 4700, 5600, 6800, 8200,
 				 10e3, 12e3, 15e3, 18e3, 22e3, 27e3, 33e3, 39e3, 47e3, 56e3, 68e3, 82e3,
 				 10e4, 12e4, 15e4, 18e4, 22e4, 27e4, 33e4, 39e4, 47e4, 56e4, 68e4, 82e4,
 				 10e5};
+	count = 61;
+	conn = 'S';
+	tc = 7;
+	tcDesc = "entire E12 resistor list connected in series";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array4);
 
-
-	char conn = 'P';
-
-	/* Use function my_add to perform processing and calculation */
-	float resultingResistance = calc_resistance(count, conn, array); 
-		
-	printf ("The resulting resistance is: %f\n", resultingResistance);
+	/* Test case 8: entire E12 resistor list connected in paralell*/
+	expectedResult = 1.764631;
+	count = 61;
+	conn = 'P';
+	tc = 8;
+	tcDesc = "entire E12 resistor list connected in paralell";
+	tcStatus[tc-1] = runTestCase(tc, tcDesc, expectedResult, count, conn, array4);
 	
-	/* Cleanup, free memory and exit */
-	//free (array);
-	return 0;
+	int i, tcSuccesses = 0, tcFailures = 0;
+	
+	for (i=0;i<tcCount;i++) {
+		
+		if (tcStatus[i] == 1)
+			tcSuccesses++;
+		else if (tcStatus[i] == 0)
+			tcFailures++;
+		
+	}
+
+	printf("Summary: after testing %d test cases, %d succeded and %d failed!\n", tcCount, tcSuccesses, tcFailures);
+
+}
+
+int runTestCase ( int testCase, char* testCaseDesc, float expectedResult, int count, char conn, float *array ) {
+
+	/* Use function calc_resistance to perform processing and calculation */
+	float resultingResistance = calc_resistance(count, conn, array); 
+	char *result;
+	int passed;
+	float rresult = roundf(resultingResistance * 1000000) / 1000000;
+
+	if ( rresult == expectedResult ) {
+		result = "passed";
+		passed=1;
+	}
+	
+	else {
+		result = "failed";
+		passed=0;
+
+	}
+	
+	printf ("\nExecuting testcase #%d: %s\n", testCase, testCaseDesc);
+	printf ("Expected result was %.4f, actual result was %.4f\n", expectedResult, rresult);
+	printf ("Test case #%d %s\n", testCase, result);
+
+	return passed;
 
 }
